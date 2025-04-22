6f39c800d5f94c3bfc42f771552901d7e38e5066
colserra <mail@colserra.dev>
2025-04-22 12:25:13 +0200

    Added more robust logging and error handling

--- src/main/java/org/queercraft/timePlayed/TimePlayed.java
+++ src/main/java/org/queercraft/timePlayed/TimePlayed.java
@@ -1,6 +1,9 @@
 package org.queercraft.timePlayed;
 
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Optional;
 /*
 * THIS PLUGIN WAS BROUGHT TO YOU BY A SEVERE CASE OF STREP THROAT,
@@ -22,13 +25,21 @@ public final class TimePlayed extends JavaPlugin {
                 Optional<QueryAPIAccessor> queryAPIOptional = new PlanHook().hookIntoPlan();
                 if (queryAPIOptional.isPresent()) {
                     queryAPI = queryAPIOptional.get();
-                    getCommand("playtime").setExecutor(new command(queryAPI));
+                    Objects.requireNonNull(getCommand("playtime")).setExecutor(new command(queryAPI));
                 } else {
                     getLogger().warning("Failed to hook into Plan, disabling TimePlayed...");
                     getServer().getPluginManager().disablePlugin(this);
                 }
             } catch (NoClassDefFoundError planIsNotInstalled) {
                 getLogger().warning("Could not find a Plan installation, disabling TimePlayed...");
+                getServer().getPluginManager().disablePlugin(this);
+            } catch (Exception e) {
+                getLogger().severe("An unexpected error occurred while enabling TimePlayed:");
+                getLogger().severe("Exception type: " + e.getClass().getName());
+                getLogger().severe("Message: " + e.getMessage());
+                Arrays.stream(e.getStackTrace())
+                        .map(StackTraceElement::toString)
+                        .forEach(stackTraceLine -> getLogger().severe("    at " + stackTraceLine));
                 getServer().getPluginManager().disablePlugin(this);
             }
         }, 600L);
